31168d93e3d5824836ce7c50ef4255c7386e601b
peeter <peetertarvas@gmail.com>
2025-07-13 23:44:02 +0300

    update pipeline logic and spring boot

---
+++ totext_gateway/totext/src/main/kotlin/com/totext/totext/TotextApplication.kt
@@ -0,0 +1,11 @@
+package com.totext.totext
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TotextApplication
+
+fun main(args: Array<String>) {
+	runApplication<TotextApplication>(*args)
+}
---
+++ totext_gateway/totext/src/test/kotlin/com/totext/totext/TotextApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.totext.totext
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TotextApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
